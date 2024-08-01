557924c7d9c0cb2d4daa654ceab3f3fd7866f342
GyeongJoon <jjangjoon328@naver.com>
2024-08-01 09:55:57 +0900

    lombok and refactoring

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,12 +2,14 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.firstproject.repository.ArticleRepository;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired // 스프링 부트가 미리 생성해 높은 리파지터리 객체 주입(DI)
@@ -19,10 +21,12 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+//        System.out.println(form.toString());
         Article article = form.toEntity();
         Article saved =  articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
         return "";
     }
 }
--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,26 +3,29 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
-    @GetMapping("/members/new")
-    public String signUp() {
+    @GetMapping("signup")
+    public String signUpPage() {
         return "/members/new";
     }
-
     @PostMapping("/join")
     public String createMember(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+//        System.out.println(form.toString());
         Member member = form.toEntity();
-        Member saved =  memberRepository.save(member);
-        System.out.println(saved.toString());
+        Member saved = memberRepository.save(member);
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
         return "";
     }
 }
