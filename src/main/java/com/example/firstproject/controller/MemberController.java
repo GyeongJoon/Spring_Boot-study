77059944c9126e08b4cb919fc1e5299f1db4ab07
GyeongJoon <jjangjoon328@naver.com>
2024-08-06 08:17:55 +0900

    Updata

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -46,4 +46,26 @@ public class MemberController {
         model.addAttribute("memberList", memberEntity);
         return "members/index";
     }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member",memberEntity);
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form) {
+        // 1. DTO를 엔티티로 변화
+        Member memberEntity = form.toEntity();
+        // 2. 엔티티를 DB에 저장
+        // 2-1. DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            memberRepository.save(memberEntity);
+        }
+        // 3. 수정 결과 페이지 리다이렉트
+        return "redirect:/members/" + memberEntity.getId();
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(id, title, content) VALUES(1, '핸드폰', '갤럭시23+');
 INSERT INTO article(id, title, content) VALUES(2, '노트북', '갤럭시 이온');
 INSERT INTO article(id, title, content) VALUES(3, '이어폰', '에어팟 프로2');
+
+INSERT INTO member(id, email, password) VALUES(1, 'jjangjoon328@naver.com', '123');
+INSERT INTO member(id, email, password) VALUES(2, 'jjanggu@naver.com', '456');
+INSERT INTO member(id, email, password) VALUES(3, 'jjangah@naver.com', '789');
