572c60e0794fd551ca346f1566dc14ffa5eefa4b
GyeongJoon <jjangjoon328@naver.com>
2024-08-06 14:03:14 +0900

    CRUD and SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(id, title, content) VALUES(1, '핸드폰', '갤럭시23+');
-INSERT INTO article(id, title, content) VALUES(2, '노트북', '갤럭시 이온');
-INSERT INTO article(id, title, content) VALUES(3, '이어폰', '에어팟 프로2');
+INSERT INTO article(title, content) VALUES('핸드폰', '갤럭시23+');
+INSERT INTO article(title, content) VALUES('노트북', '갤럭시 이온');
+INSERT INTO article(title, content) VALUES('이어폰', '에어팟 프로2');
 
 INSERT INTO member(id, email, password) VALUES(1, 'jjangjoon328@naver.com', '123');
 INSERT INTO member(id, email, password) VALUES(2, 'jjanggu@naver.com', '456');
