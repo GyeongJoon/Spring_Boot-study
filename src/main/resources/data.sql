5a87b85681baa4fd6fafa6f5b0b30a373d5f9888
GyeongJoon <jjangjoon328@naver.com>
2024-08-08 13:04:03 +0900

    test code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(title, content) VALUES('핸드폰', '갤럭시23+');
-INSERT INTO article(title, content) VALUES('노트북', '갤럭시 이온');
-INSERT INTO article(title, content) VALUES('이어폰', '에어팟 프로2');
+INSERT INTO article(title, content) VALUES('소주', '새로');
+INSERT INTO article(title, content) VALUES('맥주', '테라');
+INSERT INTO article(title, content) VALUES('막걸리', '장수막걸리');
 
 INSERT INTO member(id, email, password) VALUES(1, 'jjangjoon328@naver.com', '123');
 INSERT INTO member(id, email, password) VALUES(2, 'jjanggu@naver.com', '456');
